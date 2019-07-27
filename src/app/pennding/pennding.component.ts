9998cbd053ec2d1f35200dae0f6c20980f5cdf49
gau7777 <gauravranjan341@gmail.com>
2019-07-27 14:20:26 +0530

    Project added

---
+++ src/app/pennding/pennding.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pennding',
+  templateUrl: './pennding.component.html',
+  styleUrls: ['./pennding.component.css']
+})
+export class PenndingComponent implements OnInit {
+  // serverId: number = 10;
+  // serverName: string = 'offline';
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // getServerStatus(){
+  //   return this.serverName;
+  // }
+
+}
